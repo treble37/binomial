6e86894af9426dc8661fab0d9e2aca2ea2e0162f
Kyle Burton <kburton@relaynetwork.com>
2012-12-01 12:55:59 -0500

    paramter validation tests

--- lib/binomial/calculator.rb
+++ lib/binomial/calculator.rb
@@ -11,6 +11,26 @@ module Binomial
       @trials = params[:trials]
       @probability = params[:probability]
       @target = params[:target]
+
+      if @trials <= 0
+        raise "Error: trials[#{@trials}] must be > 0"
+      end
+
+      if @target <= 0
+        raise "Error: target[#{@target}] must be > 0"
+      end
+
+      if @probability <= 0
+        raise "Error: probability[#{@probability}] must be > 0"
+      end
+
+      if @probability > 1
+        raise "Error: probability[#{@probability}] must be <= 1"
+      end
+
+      if @trials < @target
+        raise "Error: target[#{@target}] must be < trials[#{@trials}]"
+      end
     end
 
     def model
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,75 @@
+require 'binomial'
+
+describe "Binomial::Calculator" do
+  describe "calculate" do
+    it "should validate probability is > 0" do
+      expect {
+        Binomial::Calculator.new(
+          :trials       => 10,
+          :probability  => 0,
+          :target       => 3
+        ).calculate
+      }.to raise_error
+    end
+
+    it "should validate probability is < 1" do
+      expect {
+        Binomial::Calculator.new(
+          :trials       => 10,
+          :probability  => 1.1,
+          :target       => 3
+        ).calculate
+      }.to raise_error
+    end
+
+    it "should succeed if probability is >0 and <1" do
+      expect {
+        Binomial::Calculator.new(
+          :trials       => 10,
+          :probability  => 0.2,
+          :target       => 3
+        ).calculate
+      }.to_not raise_error
+    end
+
+    it "should validate target < trials" do
+      expect {
+        Binomial::Calculator.new(
+          :trials       => 10,
+          :probability  => 0.2,
+          :target       => 11
+        ).calculate
+      }.to raise_error
+    end
+
+    it "should succeed if target < trials" do
+      expect {
+        Binomial::Calculator.new(
+          :trials       => 10,
+          :probability  => 0.2,
+          :target       => 3
+        ).calculate
+      }.to_not raise_error
+    end
+
+    it "should validate target > 0" do
+      expect {
+        Binomial::Calculator.new(
+          :trials       => 10,
+          :probability  => 0.2,
+          :target       => 0
+        ).calculate
+      }.to raise_error
+    end
+
+    it "should succeed if target > 0" do
+      expect {
+        Binomial::Calculator.new(
+          :trials       => 10,
+          :probability  => 0.2,
+          :target       => 3
+        ).calculate
+      }.to_not raise_error
+    end
+  end
+end
